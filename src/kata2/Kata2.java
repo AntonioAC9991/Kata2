cd6aeaebaf5146936b45a3374ce2b5a1134c642a
Antonio <tonyaguado99@gmail.com>
2021-01-12 12:02:14 +0000

    Kata 2 finalizada

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,18 @@
+package kata2;
+
+import java.util.Map;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        
+        String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
+        Histogram histo = new Histogram(data);
+        
+        Map<String, Integer> histogr = histo.getHistogram();
+        
+        for (Map.Entry<String, Integer> entry : histogr.entrySet()) {
+            System.out.println(entry.getKey() + " ==> " + entry.getValue());
+        }        
+    }
+}
