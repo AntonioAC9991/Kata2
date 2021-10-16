c3b33e399f6ac400d22a7e5a2770a63cd4e4659a
Antonio <tonyaguado99@gmail.com>
2021-10-16 20:07:46 +0100

    Kata2 Parte 1 finalizada

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,7 +5,24 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Kata2 {
-
+    
+    public static void main(String[] args) {
+        int[] data = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+        HashMap<Integer, Integer> histogram = new HashMap<Integer,Integer>();
+               
+        for (int i = 0; i < data.length; i++) {
+            if(histogram.containsKey(data[i])) {
+                histogram.put(data[i], histogram.get(data[i])+1);
+            } else {
+                histogram.put(data[i], 1);
+            }
+        }
+        for (Integer key : histogram.keySet()) {
+            System.out.println(key + "==>" + histogram.get(key));            
+        }
+    }
+    
+    /**
     public static void main(String[] args) {
         HashMap<Integer, Integer> hashMap = new HashMap<Integer,Integer>();
         
@@ -45,12 +62,14 @@ public class Kata2 {
             Integer key = (Integer)iterator2.next().getKey();
             Integer value = (Integer)iterator2.next().getValue();
         }
-        */
+        
         ///////////////ESTE ES EL PEOR MÉTODO/////////////////////////////////////////////
         //Método 4
         for (Integer clave : hashMap.keySet()) {
             System.out.println("La clave " + clave + " corresponde al valor " + hashMap.get(clave));
             
         }
+        
     } 
+    */
 }
